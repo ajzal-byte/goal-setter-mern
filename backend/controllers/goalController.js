c6152c7b1bdd5098dd5be4078cd3838ea7f89f7a
ajzal-byte <ajzalaju10@gmail.com>
2024-03-14 19:41:27 +0530

    controller implemented

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,32 @@
+// @desc Get goals
+// @route GET /api/goals
+// @acess Private
+const getGoals = (req, res) => {
+  // res.status(200).json({message: "Get goals"});
+  console.log(req.body);
+}
+
+// @desc Set goal
+// @route POST /api/goals
+// @acess Private
+const setGoal = (req, res) => {
+  res.status(200).json({message: "Set goal"});
+}
+
+// @desc Update goal
+// @route PUT /api/goals/:id
+// @acess Private
+const updateGoal = (req, res) => {
+  res.status(200).json({message: `Update goal: ${req.params.id}`});
+}
+
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @acess Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({message: `Delete goal: ${req.params.id}`});
+}
+
+module.exports = {
+  getGoals, setGoal, updateGoal, deleteGoal
+}
