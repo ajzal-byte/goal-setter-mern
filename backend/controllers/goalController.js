b550896151c6ee54ad55a16924115141d6f2d0e4
ajzal-byte <ajzalaju10@gmail.com>
2024-03-14 19:45:48 +0530

    code formatted 'prettier'

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -4,29 +4,32 @@
 const getGoals = (req, res) => {
   // res.status(200).json({message: "Get goals"});
   console.log(req.body);
-}
+};
 
 // @desc Set goal
 // @route POST /api/goals
 // @acess Private
 const setGoal = (req, res) => {
-  res.status(200).json({message: "Set goal"});
-}
+  res.status(200).json({ message: "Set goal" });
+};
 
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @acess Private
 const updateGoal = (req, res) => {
-  res.status(200).json({message: `Update goal: ${req.params.id}`});
-}
+  res.status(200).json({ message: `Update goal: ${req.params.id}` });
+};
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @acess Private
 const deleteGoal = (req, res) => {
-  res.status(200).json({message: `Delete goal: ${req.params.id}`});
-}
+  res.status(200).json({ message: `Delete goal: ${req.params.id}` });
+};
 
 module.exports = {
-  getGoals, setGoal, updateGoal, deleteGoal
-}
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
