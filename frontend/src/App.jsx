1ae0db0481e881d9186c67dbe26362839fb4488f
ajzal-byte <ajzalaju10@gmail.com>
2024-03-15 21:48:07 +0530

    Header and Navigation implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Dashboard, Login, Register } from "./pages";
-function App() {
+import { Header } from "./components";
+
+const App = () => {
   return (
     <>
       <Router>
         <div className="container">
+          <Header/>
           <Routes>
             <Route path="/" element={<Dashboard/>} />
             <Route path="/login" element={<Login/>} />
