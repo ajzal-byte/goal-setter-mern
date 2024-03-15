b9f9122fae39991a94be433ca3e3bac174576415
ajzal-byte <ajzalaju10@gmail.com>
2024-03-15 15:11:57 +0530

    auth implemented in the route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,8 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @acess Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "user data" });
+  const { _id, name, email } = await User.findById(req.user.id);
+  res.status(200).json({id: _id, name, email})
 });
 
 // Generate JWT
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -25,11 +25,11 @@ const protect = asyncHandler(async (req, res, next) => {
       res.status(401);
       throw new Error("Not authorized");
     }
+  }
 
-    if (!token) {
-      res.status(401);
-      throw new Error("Not authorized, no token");
-    }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
 });
 
