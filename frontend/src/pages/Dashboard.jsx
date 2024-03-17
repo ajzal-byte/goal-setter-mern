6052868eff72044f73f025e2519da8ffc63ca6a1
ajzal-byte <ajzalaju10@gmail.com>
2024-03-17 17:08:52 +0530

    fetch golas from server

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,39 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { GoalForm } from "../components";
+import { Spinner } from "../components";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
     if (!user) navigate("/login");
-  }, [user, navigate]);
+
+    if (isError) console.log(message);
+
+
+    dispatch(getGoals());
+
+    return () => dispatch(reset)
+  }, [user, navigate, isError, message, dispatch]);
+
+  if(isLoading) return <Spinner/>
+
   return (
     <>
       <section>
-        <h1>Welcome welcome welcome {user.name}</h1>
+        <h1>Welcome welcome welcome {user?.name}</h1>
         <p>Goals Dashboard</p>
       </section>
-      <GoalForm/>
+      <GoalForm />
     </>
   );
 };
