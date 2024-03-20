9d57b5003480008145208e0d913cc2e56024b216
ajzal-byte <ajzalaju10@gmail.com>
2024-03-21 00:29:05 +0530

    users list imported

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,6 @@ import Header from "./Header";
 import Spinner from "./Spinner";
 import GoalForm from "./GoalForm";
 import GoalItem from "./GoalItem";
+import UsersList from "./UsersList";
 
-export { Header, Spinner, GoalForm, GoalItem };
+export { Header, Spinner, GoalForm, GoalItem, UsersList };
