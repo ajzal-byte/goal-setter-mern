627d4f31cdfa8eb050df5108f8496e35dff6f63c
ajzal-byte <ajzalaju10@gmail.com>
2024-03-20 19:13:22 +0530

    adminAuth implemented

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -33,4 +33,35 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = { protect };
+const protectAdmin = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get admin from the token
+      req.admin = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = { protect, protectAdmin };
