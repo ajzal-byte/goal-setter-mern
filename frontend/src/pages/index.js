5207bfacfcf9afd58d065ed1bccd992d1c5b3277
ajzal-byte <ajzalaju10@gmail.com>
2024-03-20 21:26:38 +0530

    admin Login and Dashboard imported to index

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,5 +2,7 @@ import Register from "./Register";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import Profile from "./Profile";
+import AdminLogin from "./admin/AdminLogin";
+import AdminDashboard from "./admin/AdminDashboard";
 
-export { Register, Login, Dashboard, Profile };
+export { Register, Login, Dashboard, Profile, AdminLogin, AdminDashboard };
