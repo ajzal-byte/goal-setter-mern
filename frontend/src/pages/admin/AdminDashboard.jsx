5947fb60d9a40c161a3340797076ba48605be5af
ajzal-byte <ajzalaju10@gmail.com>
2024-03-20 21:41:30 +0530

    dashboard ui modified

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { adminLogout, reset } from "../../features/adminAuth/adminAuthSlice";
+import { FaSearch } from "react-icons/fa";
 
 const AdminDashboard = () => {
-  return (
-    <div>AdminDashboard</div>
-  )
-}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { admin } = useSelector((state) => state.adminAuth);
 
-export default AdminDashboard
+  const onLogout = () => {
+    dispatch(adminLogout());
+    dispatch(reset());
+    navigate("/admin/login");
+  };
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    if (!admin) navigate("/admin/login");
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [dispatch, admin, navigate, searchQuery]);
+
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+    setSearchQuery(e.target.value)
+  }
+
+  const onAddUser = (e) => {
+    e.preventDefault();
+    // navigate("/admin/adduser")
+  }
+  return (
+    <div className="container-1">
+      <div className="nav">
+        <h3>Admin Dashboard</h3>
+
+        <div style={{ display: "flex" }} className="form-group">
+          <input
+            style={{ height: "35px" }}
+            className="form-control"
+            placeholder="username/email"
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+          <button
+            style={{ height: "35px", marginLeft: "10px" }}
+            className="btn-1"
+          >
+            {" "}
+            <FaSearch /> Search
+          </button>
+        </div>
+      </div>
+
+      <div className="profile">
+        <div className="profile-image">
+          <img
+            src={
+              admin?.profileUrl
+                ? admin.profileUrl
+                : "https://static.vecteezy.com/system/resources/thumbnails/002/387/693/small/user-profile-icon-free-vector.jpg"
+            }
+            alt="profile"
+          />{" "}
+        </div>
+        <div className="profile-card">
+          <div className="profile-info">
+            <p>Name : {admin && admin.name}</p>
+            <p> Email : {admin && admin.email}</p>
+          </div>
+          <div className="profile-buttons">
+            <button onClick={onAddUser} className="btn-1">
+              {" "}
+              Add User
+            </button>
+            <button onClick={onLogout} className="btn">
+              {" "}
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* <UserList/> */}
+    </div>
+  );
+};
+
+export default AdminDashboard;
